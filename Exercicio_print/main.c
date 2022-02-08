11b70f116e76da62dfd16d525cb3c25287591afa
Thatiane Deboleto <drathatianenimtzz@icloud.com>
2022-02-08 16:23:24 -0300

    exercicios aula log de programacao

---
+++ Exercicio_print/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Bem-vindos ao \/Mundo\\ da programacao em \"C\" ");
+    return 0;
+}
---
+++ Exercicios_erros/main.c
@@ -0,0 +1,83 @@
+/*
+* Copyright Fatec Mogi Mirim
+1- Main = main
+*/
+#include <stdio.h>
+main()
+{
+    printf("Ola mundo!\n");
+}
+
+/*
+Copyright Fatec Mogi Mirim
+2- Faltou () após o main
+*/
+#include <stdio.h>
+main()
+{
+    printf("Ola mundo!\n");
+}
+/*
+Copyright Fatec Mogi Mirim
+2- Faltou printf
+*/
+#include <stdio.h>
+main()
+{
+    print("Ola mundo!\n");
+}
+/*
+* Copyright Fatec Mogi Mirim
+3- Faltou outro printf com espaço entre __mundo
+*/
+#include <stdio.h>
+main()
+{
+printf("Ola")("mundo!\n");
+}
+
+/*
+* Copyright Fatec Mogi Mirim
+4- Erro no comentario /*
+*/
+#include <stdio.h>
+main()
+{
+    printf("Ola mundo!\n");
+}
+/*
+/* Copyright Fatec Mogi Mirim
+5- Sinal a mais de comentario */
+*/
+#include <stdio.h>
+main()
+{
+    printf("Ola mundo!\n");
+}
+/*
+* Copyright Fatec Mogi Mirim
+6- Falta aspas no print
+*/
+#include <stdio.h>
+main()
+{
+    printf(Ola mundo!\n);
+}
+/*
+* Copyright Fatec Mogi Mirim
+7- Falta ; apos printf
+*/
+#include <stdio.h>
+main()
+{
+    printf("Ola mundo!\n")
+}
+/*
+* Copyright Fatec Mogi Mirim
+8- Falta # antes do include
+*/
+include <stdio.h>
+main()
+{
+    printf("Ola mundo!\n");
+}
