2cd389f90f9e8b854ce868ddb50e86fe059025ba
Thatiane Deboleto <drathatianenimtzz@icloud.com>
2022-02-15 15:14:13 -0300

    add projeto linguagem C- Aula 2

---
+++ Aula2/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Aprendendo algoritmo\n");
+    printf("Algoritmo eh legal!\n Ele nem eh tao dificil!");
+    return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Total   100%%\nIVA      17%%\nIRS      15%%\nLiquido   68%%");
+    return 0;
+}
+/*
+4-Os comentarios devem ser utilizados para designar toda função escrita no programa.
+5-letra "d".
+6-Letra "d".
+7- a-falso, b-verdadeiro,c-verdadeiro,d-verdadeiro, e-verdadeiro, f-falso, g-falso
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Total   100%%\nIVA      17%%\nIRS      15%%\nLiquido   68%%");
+    return 0;
+}
+/*
+Thatiane Deboleto
+15/02/2022
+10:22 am
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Thatiane \n 26 anos \n Sao Paulo \n");
+    return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("O \\n Pula uma linha no codigo\n");
+    printf("O \\\\ imprime uma barra invertida \n");
+    printf("O \\t faz uma tabulacao vertical \n");
+    printf("O %%%%, imprime a porcentagem");
+
+    return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hoje o dia esta ensolarado, com 25 Graus Celsius");
+    return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tNUM lugar da Mancha, de cujo nome nao quero lembrar-me, vivia, nao ha muito, um fidalgo, dos de lanca em cabido, adarga antiga, rocim fraco, e galgo corredor.\n \n");
+    printf("\tPassadio, olha seu tanto mais de vaca do que de carneiro, as mais das ceias restos da carne picados com sua cebola e vinagre, aos sabados outros sobejos ainda somenos, lentilhas as sextas-feiras, algum pombito de crescenca aos domingos, consumiam tres quartos do seu haver. O remanescente, levavam-no saio de belarte, calcas de veludo para as festas, com seus pantufos do mesmo; e para os dias de semana o seu bellori do mais fino.\n \n");
+    printf("(Dom Quichote de La Mancha – Cervantes)");
+    return 0;
+}
